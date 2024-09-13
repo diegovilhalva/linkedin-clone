d807df861424e850eac16152485015d36a7210b9
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-13 09:08:57 -0300

    backend setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+
+export const signUp = (req,res) => {
+    
+}
+
+export const login = (req,res) => {
+
+}
+
+
+export const logout = (req,res) => {
+
+}
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
 import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js"
+
+dotenv.config()
+const app = express()
+
+app.use("/api/v1/auth", authRoutes)
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`)
+    connectDB()
+})
