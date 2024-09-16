69bb1e768c21a737fef0967cf426cb34b3190266
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-16 11:38:37 -0300

    posts,notification created

--- backend/emails/emailHandlers.js
+++ backend/emails/emailHandlers.js
@@ -1,5 +1,5 @@
 import { mailTrapClient, sender } from "../lib/mailtrap.js"
-import { createWelcomeEmailTemplate } from "./emailTemplates.js"
+import { createCommentNotificationEmailTemplate, createWelcomeEmailTemplate } from "./emailTemplates.js"
 
 export const sendWelcomeEmail = async (email, name, profileUrl) => {
     const recepient = [{ email }]
@@ -13,8 +13,26 @@ export const sendWelcomeEmail = async (email, name, profileUrl) => {
             category: "welcome"
         })
 
-        console.log("Email enviado com sucesso",response)
+        console.log("Email enviado com sucesso", response)
     } catch (error) {
         throw new Error(error.message)
     }
+}
+
+export const sendCommentNotificationEmail = async (recipientEmail, recepientName, commenterName, postUrl, commentContent) => {
+    const recipient = [{ email }]
+
+    try {
+        const response = mailTrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Novo comentário em sua publicação",
+            html: createCommentNotificationEmailTemplate(recepientName, commenterName, postUrl, commentContent),
+            category:"comment_notification"
+        })
+    } catch (error) {
+        throw new Error(error.message)
+        
+    }
+
 }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 import { connectDB } from "./lib/db.js"
 
 dotenv.config()
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users",userRoutes)
+app.use("/api/v1/posts",postRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
