bd555271f8a63f4fbd805a231d60ff69c0e5e25d
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-16 09:33:00 -0300

    user routes created and coundinary setup

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
 import { connectDB } from "./lib/db.js"
 
 dotenv.config()
@@ -10,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
-
+app.use("/api/v1/users",userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
