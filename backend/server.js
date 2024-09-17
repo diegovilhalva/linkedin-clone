a2843816c59ef50b522ca62fa7b8e0e7c90a96fa
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-17 08:52:06 -0300

    frontend setup

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,16 @@ import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
 import notificationRoutes from "./routes/notification.route.js"
 import connectionsRoutes from "./routes/connection.route.js"
+import cors  from "cors"
 import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 const app = express()
-
-app.use(express.json())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
+app.use(express.json({limit:"5mb"}))
 app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users",userRoutes)
