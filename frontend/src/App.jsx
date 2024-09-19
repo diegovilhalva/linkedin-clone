2d13a5e88a9911005a04bd1cf042ac831d91f7f2
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-19 10:41:12 -0300

    single post page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import { useQuery } from "@tanstack/react-query"
 import { axiosInstance } from "./lib/axios"
 import NotificationsPage from "./pages/NotificationsPage"
 import NetworkPage from "./pages/NetworkPage"
+import PostPage from "./pages/PostPage"
 
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
         <Route path="/login" element={!authUser ?<LoginPage />:<Navigate to={'/'} />} />
         <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to={"/login"} /> } />
         <Route path="/network" element={authUser ? <NetworkPage/> : <Navigate to={"/login"} />} />
+        <Route path="/post/:postId" element={authUser ? <PostPage />: <Navigate to={"/login"} />} />
       </Routes>
       <Toaster />
     </Layout>
