c5404859532d7975471ae57f4052facfe227629a
Diego Vilhalva <diegorocha450@yahoo.com>
2024-09-20 09:15:49 -0300

    preparing to deploy

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,35 @@ import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
 import notificationRoutes from "./routes/notification.route.js"
 import connectionsRoutes from "./routes/connection.route.js"
-import cors  from "cors"
+import cors from "cors"
+import path from 'path'
 import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 const app = express()
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}))
-app.use(express.json({limit:"5mb"}))
+const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }))
+}
+app.use(express.json({ limit: "5mb" }))
 app.use(cookieParser())
 app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/users",userRoutes)
-app.use("/api/v1/posts",postRoutes)
-app.use("/api/v1/notifications",notificationRoutes)
-app.use("/api/v1/connections",connectionsRoutes)
-const PORT = process.env.PORT || 5000
+app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/posts", postRoutes)
+app.use("/api/v1/notifications", notificationRoutes)
+app.use("/api/v1/connections", connectionsRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"/frontend/dist")))
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+    })
+}
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta: ${PORT}`)
     connectDB()
